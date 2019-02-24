d49a02bd57efed96b43e0ccdf3a18aa37407c8c8
grace <gracetheguy@gmail.com>
2019-02-24 13:20:52 +0000

    done alot of things

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     date= models.DateTimeField()
     text= models.TextField(max_length=200)
     image= models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.text[:100]
+
+    def date_format(self):
+        return self.date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
